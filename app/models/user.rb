c1b298de874e5cee996110524add36e1a8d7ee54
Charity Fuller <charryef@gmail.com>
2018-12-28 01:11:16 -0800

    Updated credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  before_save { self.email = email.downcase if email.present? }
+  # before_save { self.email = email.downcase if email.present? }
 
-  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, if: -> {password_digest.nil?}
-  validates :password, length: { minimum: 6 }, allow_blank: true
-  validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
+  # validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  # validates :password, presence: true, length: { minimum: 6 }, if: -> {password_digest.nil?}
+  # validates :password, length: { minimum: 6 }, allow_blank: true
+  # validates :email,
+  #            presence: true,
+  #            uniqueness: { case_sensitive: false },
+  #            length: { minimum: 3, maximum: 254 }
+
 
 end
