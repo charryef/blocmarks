5058c4bed5c177d971f4e977800867d4ef9d5ab4
Charity Fuller <charryef@gmail.com>
2019-01-07 16:17:59 -0800

    Updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = "cfuller-blocmarks.herokuapp.com"
+  config.action_mailer.default_url_options = { host: 'cfuller-blocmarks.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
