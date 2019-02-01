6c2c2d9645d087a46f4b90c40687a04a164a4316
Charity Fuller <charryef@gmail.com>
2019-02-01 09:24:22 -0800

    Fixing mailgun

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @user_topics = @user.topics
+    @user_bookmarks =@user.bookmarks
     @liked_bookmarks = @user.likes
   end
 end
